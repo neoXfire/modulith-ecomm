f92140dc511745528a08315a291133be1bb60cdf
neoXfire <tavares.florian@outlook.fr>
2025-02-11 21:33:21 +0100

    Add modularity test

--- src/test/java/fr/boursorama/modulith/ecomm/ModulithEcommApplicationTests.java
+++ src/test/java/fr/boursorama/modulith/ecomm/ModulithEcommApplicationTests.java
@@ -1,15 +1,26 @@
 package fr.boursorama.modulith.ecomm;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class ModulithEcommApplicationTests {
 
     @Test
     @DisplayName("Ensure the complete application context is loaded")
+    @Disabled
     void contextLoads() {
     }
+
+    @Test
+    void checkModularity() {
+        ApplicationModules modules = ApplicationModules.of(ModulithEcommApplication.class);
+        modules.verify();
+    }
+
+
 
 }
